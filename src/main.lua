3bf54abba6e4fe901447cdcdd860b1210d2ad784
Kai Hossbach <paranoiax89@googlemail.com>
2012-08-21 12:15:55 +0200

    alpha 1.0.0
    -added matchball behaviour
    -added additional gamelogic
    -improved overall physics and controls
    -improved AI to be less perfect
    -removed controls for second player (for now)

--- src/main.lua
+++ src/main.lua
@@ -55,29 +55,30 @@ function love.load()
 	objects.ball.fixture:setCategory(2)
 	objects.ball.fixture:setMask(1)
 	objects.ball.fixture:setRestitution(0.8)
-	objects.ball.body:setMass(0.25)
-	objects.ball.body:setGravityScale(1.5)
+	objects.ball.body:setMass(0.215)
+	objects.ball.body:setGravityScale(2.0)
+	objects.ball.body:setLinearDamping(1.5)
 	
 	objects.player1 = {}
 	objects.player1.body = love.physics.newBody(world, screenWidth / 8 * 3, screenHeight - 50, "dynamic")
 	objects.player1.shape = love.physics.newCircleShape(28)
 	objects.player1.fixture = love.physics.newFixture(objects.player1.body, objects.player1.shape, 1)
 	objects.player1.fixture:setCategory(3)
-	objects.player1.body:setGravityScale(2)	
+	objects.player1.body:setGravityScale(6.5)	
 	objects.player1.fixture:setRestitution(0.2)
 	objects.player1.body:setFixedRotation(true)
-	objects.player1.body:setLinearDamping(0.3)
+	objects.player1.body:setLinearDamping(4)
 	
 	objects.player2 = {}
 	objects.player2.body = love.physics.newBody(world, screenWidth - screenWidth / 8 * 3, screenHeight - 50, "dynamic")
 	objects.player2.shape = love.physics.newCircleShape(28)
 	objects.player2.fixture = love.physics.newFixture(objects.player2.body, objects.player2.shape, 1)
 	objects.player2.fixture:setCategory(3)
-	objects.player2.body:setGravityScale(2)
+	objects.player2.body:setGravityScale(6.5)
 	objects.player2.fixture:setRestitution(0.2)
 	objects.player2.body:setFixedRotation(true)
-	objects.player2.body:setLinearDamping(0.3)
-	objects.player2.fixture:setFriction(1.5) -- AI ONLY
+	objects.player2.body:setLinearDamping(1.5)
+	objects.player2.fixture:setFriction(1.15) -- AI ONLY
 	
 	objects.player1.score = 0
 	objects.player2.score = 0
@@ -86,6 +87,8 @@ function love.load()
 	objects.player2.canJumpAgain = 0	
 	
 	startgame = true
+	winner = 0
+	maxpoints = 3
 	
 end
 
@@ -94,9 +97,9 @@ function love.update(dt)
 	world:update(dt)
 	
 	if startgame == true then
-		objects.ball.body:setPosition(screenWidth / 2, screenHeight / 2 - 170)
+		objects.ball.body:setPosition(screenWidth / 2, screenHeight / 2 - 170)		
 		objects.ball.body:setAngularVelocity(0,0)
-		objects.ball.body:setLinearVelocity(0,0)		
+		objects.ball.body:setLinearVelocity(0,0)
 		objects.player1.body:setLinearVelocity(0,0)
 		objects.player2.body:setLinearVelocity(0,0)
 		objects.player2.body:setPosition(screenWidth - screenWidth / 8 * 3, screenHeight - 50)
@@ -106,10 +109,10 @@ function love.update(dt)
 	end
 	
 	if begin_player_1 == true then
-		objects.ball.body:applyLinearImpulse(27, 0)
+		objects.ball.body:applyLinearImpulse(44, 0)
 		begin_player_1 = false
 	elseif begin_player_2 == true then
-		objects.ball.body:applyLinearImpulse(-27, 0)
+		objects.ball.body:applyLinearImpulse(-44, 0)
 		begin_player_2 = false
 	end
 	
@@ -123,7 +126,7 @@ function love.update(dt)
 	
 	if objects.ball.body:getY() > screenHeight - 40 then
 		if objects.ball.body:getX() > screenWidth / 2 then
-			objects.player1.score = objects.player1.score + 1			
+			objects.player1.score = objects.player1.score + 1
 			startgame = true
 			begin_player_1 = true
 		elseif objects.ball.body:getX() < screenWidth / 2 then
@@ -134,40 +137,20 @@ function love.update(dt)
 	end	
 	
 	player2_ai()
-	
-	function love.keypressed(key)
-		if objects.player1.body:getY() > screenHeight - 55 and key == "w" then
-			objects.player1.body:applyLinearImpulse(0, -500)
-		elseif objects.player1.canJumpAgain == 1 and key == "w" then
-			objects.player1.body:applyLinearImpulse(0, -200)
-			objects.player1.canJumpAgain = 0
-		elseif key == "r" then
-			startgame = true
-		end
-	end
-		
-	if love.keyboard.isDown("a") then
-		objects.player1.body:applyForce(-1300, 0)
-	elseif love.keyboard.isDown("d") then
-		objects.player1.body:applyForce(1300, 0)
-	end
-	
-	if love.keyboard.isDown("left") then
-		objects.player2.body:applyForce(-1300, 0)
-	elseif love.keyboard.isDown("right") then
-		objects.player2.body:applyForce(1300, 0)
-	end
+	controls()
 
 end
 
 function love.draw()
 
 	draw_winner()
+	draw_matchball()
 	
 	love.graphics.setColor(255,255,255)
 	love.graphics.setFont(font)
 	love.graphics.print(objects.player1.score, 20,15)
 	love.graphics.print(objects.player2.score, screenWidth -45,15)
+	--love.graphics.print(winner, screenWidth / 2,15)
 
 	love.graphics.setColor(255,255,255)
 	love.graphics.polygon("fill", objects.mesh.body:getWorldPoints(objects.mesh.shape:getPoints()))
@@ -188,26 +171,79 @@ end
 
 function player2_ai()
 	if objects.ball.body:getX() > screenWidth / 2 and objects.ball.body:getX() +20 < objects.player2.body:getX() then
-		objects.player2.body:applyForce(-4000, 0)
-	elseif objects.ball.body:getX() > screenWidth / 2 and objects.ball.body:getX() +20 > objects.player2.body:getX() then
-		objects.player2.body:applyForce(4000, 0)
+		objects.player2.body:applyForce(-1900, 0)
+	elseif objects.ball.body:getX() > screenWidth / 2 and objects.ball.body:getX() +20 > objects.player2.body:getX() and objects.ball.body:getX() < screenWidth - 81 then
+		objects.player2.body:applyForce(1900, 0)
 	end
-	--if objects.ball.body:getX() > screenWidth / 2 then
-		--objects.player2.body:setPosition(objects.ball.body:getX() + 27, objects.player2.body:getY())
-	--end
-	if objects.ball.body:getX() > screenWidth / 2 and objects.ball.body:getY() > screenHeight - screenHeight / 3 and objects.player2.body:getY() > objects.ball.body:getY() and objects.ball.body:getX() - 30 < objects.player2.body:getX() and objects.ball.body:getX() + 30 > objects.player2.body:getX() then
-		objects.player2.body:applyLinearImpulse(0, -75)
+	if objects.ball.body:getX() > screenWidth / 2 and objects.ball.body:getY() > screenHeight - screenHeight / 3 and objects.player2.body:getY() > objects.ball.body:getY() and objects.ball.body:getX() - 35 < objects.player2.body:getX() and objects.ball.body:getX() + 35 > objects.player2.body:getX() then
+		objects.player2.body:applyLinearImpulse(0, -185)
+	elseif objects.ball.body:getX() < screenWidth / 2 - 40 and objects.player2.body:getX() < screenWidth / 4 * 3 - 50 then
+		objects.player2.body:applyForce(1800, 0)
+	elseif objects.ball.body:getX() < screenWidth / 2 - 40 and objects.player2.body:getX() > screenWidth / 4 * 3 + 50 then
+		objects.player2.body:applyForce(-1800, 0)
 	end
 end
 
 function draw_winner()
-	if objects.player1.score >= 15 and objects.player1.score - objects.player2.score >= 2 then
+	if objects.player1.score >= maxpoints and objects.player1.score - objects.player2.score >= 2 then
 		love.graphics.setColor(255,255,255)
 		love.graphics.setFont(font2)
 		love.graphics.printf("Player 1 wins!", screenWidth / 4, screenHeight / 2, screenWidth / 2, 'center')
-	elseif objects.player2.score >= 15 and objects.player2.score - objects.player1.score >= 2 then
+		love.graphics.printf("Press 'R' to restart!", screenWidth / 4, screenHeight / 2 + 40, screenWidth / 2, 'center')
+		objects.ball.body:setActive(false)
+		objects.player1.body:setActive(false)
+		objects.player2.body:setActive(false)
+		begin_player_1 = false
+		begin_player_2 = false
+		winner = 1
+	elseif objects.player2.score >= maxpoints and objects.player2.score - objects.player1.score >= 2 then
 		love.graphics.setColor(255,255,255)
 		love.graphics.setFont(font2)
 		love.graphics.printf("Player 2 wins!", screenWidth / 4, screenHeight / 2, screenWidth / 2, 'center')
+		love.graphics.printf("Press 'R' to restart!", screenWidth / 4, screenHeight / 2 + 40, screenWidth / 2, 'center')
+		objects.ball.body:setActive(false)
+		objects.player1.body:setActive(false)
+		objects.player2.body:setActive(false)
+		begin_player_1 = false
+		begin_player_2 = false
+		winner = 1
+	end
+end
+
+function draw_matchball()
+	if objects.player1.score >= maxpoints -1 and objects.player1.score - objects.player2.score >= 1 and winner == 0 then
+		love.graphics.setColor(255,255,255)
+		love.graphics.setFont(font2)
+		love.graphics.printf("Player 1 MATCHBALL!", screenWidth / 4, screenHeight / 4 -80, screenWidth / 2, 'center')
+	elseif objects.player2.score >= maxpoints -1 and objects.player2.score - objects.player1.score >= 1 and winner == 0 then
+		love.graphics.setColor(255,255,255)
+		love.graphics.setFont(font2)
+		love.graphics.printf("Player 2 MATCHBALL!", screenWidth / 4, screenHeight / 4 -80, screenWidth / 2, 'center')
+	end
+end
+
+function controls()
+	function love.keypressed(key)
+		if objects.player1.body:getY() > screenHeight - 55 and key == "w" then
+			objects.player1.body:applyLinearImpulse(0, -1250)
+		elseif objects.player1.canJumpAgain == 1 and key == "w" then
+			objects.player1.body:applyLinearImpulse(0, -900)
+			objects.player1.canJumpAgain = 0
+		elseif key == "r" and winner == 1 then
+			objects.player1.score = 0
+			objects.player2.score = 0
+			objects.ball.body:setActive(true)
+			objects.player1.body:setActive(true)
+			objects.player2.body:setActive(true)
+			objects.player1.body:setLinearVelocity(0,0)
+			objects.player2.body:setLinearVelocity(0,0)
+			winner = 0
+		end
+	end
+		
+	if love.keyboard.isDown("a") then
+		objects.player1.body:applyForce(-3200, 0)
+	elseif love.keyboard.isDown("d") then
+		objects.player1.body:applyForce(3200, 0)
 	end
 end
